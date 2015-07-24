3be3b57d4d732144fac24d8d8eb073e665f1013b
dcsan <dc@pikkle.com>
2015-07-23 21:24:15 -0700

    rest api refactor into module

--- app.js
+++ app.js
@@ -2,54 +2,17 @@
 "use strict";
 
 var GitterBot = {
-  roomId: '55b1a9030fc9f982beaac901'  // botzy
+  roomId: '55b1a9030fc9f982beaac901',  // botzy
+  clientId: process.env.GITTER_KEY  // dont like this in two places
 }
 
 var express         = require('express');
 var passport        = require('passport');
-var OAuth2Strategy  = require('passport-oauth2');
-var request         = require('request');
-
-var gitterHost    = process.env.HOST || 'https://gitter.im';
 var port          = process.env.PORT || 7000;
 
+
+
 // Client OAuth configuration
-var clientId      = process.env.GITTER_KEY;
-var clientSecret  = process.env.GITTER_SECRET;
-
-// Gitter API client helper
-var gitter = {
-  fetch: function(path, token, cb) {
-    var options = {
-     url: gitterHost + path,
-     headers: {
-       'Authorization': 'Bearer ' + token
-     }
-    };
-
-    request(options, function (err, res, body) {
-      if (err) return cb(err);
-
-      if (res.statusCode === 200) {
-        cb(null, JSON.parse(body));
-      } else {
-        cb('err' + res.statusCode);
-      }
-    });
-  },
-
-  fetchCurrentUser: function(token, cb) {
-    this.fetch('/api/v1/user/', token, function(err, user) {
-      cb(err, user[0]);
-    });
-  },
-
-  fetchRooms: function(user, token, cb) {
-    this.fetch('/api/v1/user/' + user.id + '/rooms', token, function(err, rooms) {
-      cb(err, rooms);
-    });
-  }
-};
 
 var app = express();
 
@@ -68,30 +31,6 @@ app.use(passport.session());
 app.use(app.router);
 
 // Passport Configuration
-
-passport.use(new OAuth2Strategy({
-    authorizationURL:   gitterHost + '/login/oauth/authorize',
-    tokenURL:           gitterHost + '/login/oauth/token',
-    clientID:           clientId,
-    clientSecret:       clientSecret,
-    callbackURL:        '/login/callback',
-    passReqToCallback:  true
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    req.session.token = accessToken;
-    gitter.fetchCurrentUser(accessToken, function(err, user) {
-      return (err ? done(err) : done(null, user));
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, JSON.stringify(user));
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, JSON.parse(user));
-});
 
 app.get('/login', 
   passport.authenticate('oauth2')
@@ -124,7 +63,7 @@ app.get('/home', function(req, res) {
     res.render('home', {
       user: req.user, 
       token: req.session.token, 
-      clientId: clientId,
+      clientId: GitterBot.clientId,
       rooms: rooms
     });
   });
@@ -135,6 +74,8 @@ app.listen(port);
 console.log('Demo app running at http://localhost:' + port);
 
 // realtime stream
-var gitterStream = require('./lib/gitter/streamApi')
+var gitterStream = require('./lib/gitter/streamApi');
+var gitter = require('./lib/gitter/restApi')
+
 gitterStream.listenToRoom(GitterBot.roomId);
 
