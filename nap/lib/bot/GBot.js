2f7f882f7f6aa85e8bf71a3f86a497e991795c37
dcsan <dc@pikkle.com>
2015-07-28 12:05:41 -0700

    linter crap

--- nap/lib/bot/GBot.js
+++ nap/lib/bot/GBot.js
@@ -1,16 +1,17 @@
 /*jslint todo: true */
+/*jslint node: true */
 
 "use strict";
 
 var assert = require("chai").assert;
-var Gitter = require('node-gitter'),
-    GitterHelper = require('../../lib/gitter/GitterHelper');
+var Gitter = require("node-gitter"),
+    GitterHelper = require("../../lib/gitter/GitterHelper");
 
-var AppConfig = require('../../config/AppConfig'),
-    RoomData = require('../../data/RoomData'),
-    Utils = require('../../lib/utils/Utils'),
+var AppConfig = require("../../config/AppConfig"),
+    RoomData = require("../../data/RoomData"),
+    Utils = require("../../lib/utils/Utils"),
     KBase = require("../../lib/bot/KBase"),
-    BotCommands = require('../../lib/bot/BotCommands');
+    BotCommands = require("../../lib/bot/BotCommands");
 
 function clog(msg, obj) {
     Utils.clog("GBot>", msg, obj);
@@ -37,16 +38,17 @@ var GBot = {
         var roomUrl = opts.roomObj.name;
         GBot.gitter.rooms.join(roomUrl, function (err, room) {
             if (err) {
-                console.warn('Not possible to join the room: ', err, roomUrl);
-                return;
+                console.warn("Not possible to join the room: ", err, roomUrl);
+                return null; // check - will this add nulls to the list of rooms?
             }
             GBot.roomList.push(room);
             GBot.listenToRoom(room);
             var text = GBot.getAnnounceMessage(opts);
             GBot.say(text, room);
-            clog('joined> ', room.uri);
+            clog("joined> ", room.uri);
             return room;
         });
+        return false;
     },
 
     getName: function () {
@@ -99,8 +101,8 @@ var GBot = {
         var keyword, cmd, cmds, res;
 
         keyword = input.text.split(" ")[0];
-        cmds = BotCommands.cmdList.filter(function (cmd) {
-            return (cmd === keyword);
+        cmds = BotCommands.cmdList.filter(function (c) {
+            return (c === keyword);
         });
         cmd = cmds[0];
         if (cmd) {
@@ -145,7 +147,7 @@ var GBot = {
 
     // turns raw text input into a json format
     parseInput: function (message) {
-        var res, str, topic, cleanText, input;
+        var res, cleanText, input;
 
         cleanText = message.model.text;
         cleanText = cleanText.valueOf(); // get value so we avoid circular refs with input.msg
@@ -160,7 +162,7 @@ var GBot = {
         };
         // console.log("input", input)
         // res = input.text.match(/(thanks|ty|thank you) \@(.*)/i)
-        res = input.text.match(/thanks @(.*)/i)
+        res = input.text.match(/thanks @(.*)/i);
         if (res) {
             input.thanks = true;
             return input;
@@ -169,16 +171,16 @@ var GBot = {
         // console.log("res", res)
         // console.log("input", input)
 
-        // FIXME this is matching on bothelp not just help
-        if (res = input.text.match(/^(help|wiki|check|hint|tip) (.*)/)) {
-            input.topic = res[2]
+        res = input.text.match(/^(help|wiki|check|hint|tip) (.*)/);
+        if (res) {
+            input.topic = res[2];
             input.cleanTopic = input.topic.replace(" ", "-").toLowerCase();
-            input.help = true
-            input.intent = res[1]
-            return input
+            input.help = true;
+            input.intent = res[1];
+            return input;
         }
 
-        clog('input', input);
+        clog("input", input);
         return input;
     },
 
@@ -189,15 +191,14 @@ var GBot = {
         var input = this.parseInput(message);
 
         if (input.help) {
-            return this.checkHelp(input)
+            return this.checkHelp(input);
         } else if (input.thanks) {
-            return this.checkThanks(input)
+            return this.checkThanks(input);
         } else if (res = this.checkCommands(input)) {
             return res;
-        } else {
-            return;
-            // return "you said: " + input.text;
         }
+        // else
+        return null;
 
     },
 
@@ -208,7 +209,7 @@ var GBot = {
         this.roomList = this.roomList || [];
         if (this.hasAlreadyJoined(room, this.roomList)) {
             return false;
-        };
+        }
 
         clog("addToRoomList", room.name);
         this.roomList.push(room);
@@ -216,14 +217,14 @@ var GBot = {
     },
 
     // checks if a room is already in bots internal list of joined rooms
-    // this is to avoid listening twice 
+    // this is to avoid listening twice
     // see https://github.com/gitterHQ/node-gitter/issues/15
     // note this is only the bots internal tracking
     // it has no concept if the gitter API/state already thinks you're joined/listening
     hasAlreadyJoined: function (room) {
         var checks = this.roomList.filter(function (rm) {
-            rm.name == room.name;
-        })
+            return (rm.name === room.name);
+        });
         var checkOne = checks[0];
         if (checkOne) {
             Utils.warning("GBot", "hasAlreadyJoined:", checkOne);
@@ -237,20 +238,20 @@ var GBot = {
     listenToRoom: function (room) {
         // gitter.rooms.find(room.id).then(function (room) {
 
-        if (this.addToRoomList(room) == false) {
+        if (this.addToRoomList(room) === false) {
             return;
         }
 
         var chats = room.streaming().chatMessages();
         // The 'chatMessages' event is emitted on each new message
-        chats.on('chatMessages', function (message) {
+        chats.on("chatMessages", function (message) {
             // clog('message> ', message.model.text);
-            if (message.operation != "create") {
+            if (message.operation !== "create") {
                 // console.log("skip msg reply", msg);
                 return;
             }
 
-            if (message.model.fromUser.username == AppConfig.botname) {
+            if (message.model.fromUser.username === AppConfig.botname) {
                 // console.warn("skip reply to bot");
                 return;
             }
@@ -277,45 +278,47 @@ var GBot = {
             // clog("gitter.rooms", that.gitter.rooms);
             that.gitter.rooms.join(roomUrl, function (err, room) {
                 if (err) {
-                    console.warn('Not possible to join the room: ', err, roomUrl);
+                    console.warn("Not possible to join the room:", err, roomUrl);
                     return;
                 }
                 that.listenToRoom(room);
-                clog('joined> ', room.name);
+                clog("joined> ", room.name);
             });
-        })
+        });
     },
 
     // uses gitter helper to fetch the list of rooms this user is "in"
     // and then tries to listen to them
-    // this is mainly to pick up new oneOnOne conversations  
+    // this is mainly to pick up new oneOnOne conversations
     // when a user DMs the bot
     // as I can't see an event the bot would get to know about that
     // so its kind of like "polling" and currently only called from the webUI
     scanRooms: function (user, token) {
-        var user = user || this.gitter.currentUser(),
-            token = token || AppConfig.token;
+        user = user || this.gitter.currentUser();
+        token = token || AppConfig.token;
 
-        clog('user', user)
-        clog('token', token)
+        clog("user", user);
+        clog("token", token);
         var that = this;
 
         GitterHelper.fetchRooms(user, token, function (err, rooms) {
-            if (err) Utils.error("GBot", "fetchRooms", rooms);
+            if (err) {
+                Utils.error("GBot", "fetchRooms", rooms);
+            }
             clog("scanRooms.rooms", rooms);
             if (!rooms) {
-                Utils.warn("cant scanRooms")
+                Utils.warn("cant scanRooms");
                 return;
             }
             // else
             rooms.map(function (room) {
                 if (room.oneToOne) {
-                    clog("oneToOne", room.name)
-                    that.gitter.rooms.find(room.id).then(function (roomObj) {
+                    clog("oneToOne", room.name);
+                        that.gitter.rooms.find(room.id).then(function (roomObj) {
                         that.listenToRoom(roomObj);
-                    })
+                    });
                 }
-            })
+            });
         });
         // GBot.gitter.rooms.find().then(function (rooms) {
         //     clog("found rooms", rooms)
@@ -328,14 +331,15 @@ var GBot = {
         GBot.gitter.currentUser()
             .then(function (user) {
                 var list = user.rooms(function (err, obj) {
-                    clog("rooms", err, obj)
+                    clog("rooms", err, obj);
                 });
                 clog("user", user);
                 clog("list", list);
                 return (list);
-            })
+            });
     }
 
-}
+};
 
 module.exports = GBot;
+
