c00ea6313a2b5764c5282851b31fb41d3fc45e74
dcsan <dc@pikkle.com>
2015-07-29 00:24:55 -0700

    pm2 run

--- nap/bin/pm2-run.sh
+++ nap/bin/pm2-run.sh
@@ -7,7 +7,8 @@ GITTER_USER_TOKEN=${GITTER_USER_TOKEN} \
     GITTER_APP_SECRET=${GITTER_APP_SECRET} \
     LOG_LEVEL=10 \
     PORT=7891 \
-    iojs app.js
+    pm2 start app.js --name web --interpreter iojs
 
+    # iojs app.js
     # node --harmony app.js
     # nodemon -x iojs app.js
