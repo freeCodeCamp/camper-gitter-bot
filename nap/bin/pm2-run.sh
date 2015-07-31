e598ed75c4ace1c4efb3c93c93876b9cab25268b
dcsan <dc@pikkle.com>
2015-07-30 17:13:58 -0700

    env vars

--- nap/bin/pm2-run.sh
+++ nap/bin/pm2-run.sh
@@ -1,15 +1,19 @@
+
+
+
+
 source bin/credentials-camperbot.sh
 
 set -x
 
+SERVER_ENV=prod
+BOTNAME=camperbot \
+BOT_APP_HOST=bot.freecodecamp.com \
 GITTER_USER_TOKEN=${GITTER_USER_TOKEN} \
-    GITTER_APP_KEY=${GITTER_APP_KEY} \
-    GITTER_APP_SECRET=${GITTER_APP_SECRET} \
-    LOG_LEVEL=10 \
-    BOT_APP_HOST=bot.freecodecamp.com \
-    PORT=7891 \
-    SERVER_ENV=prod
-    BOTNAME=camperbot \
+GITTER_APP_KEY=${GITTER_APP_KEY} \
+GITTER_APP_SECRET=${GITTER_APP_SECRET} \
+LOG_LEVEL=10 \
+PORT=7891 \
     pm2 start app.js --name bot --interpreter iojs
 
     # iojs app.js
--- nap/bin/run-dev-bothelp.sh
+++ nap/bin/run-dev-bothelp.sh
@@ -1,17 +1,19 @@
+
+
 # bin/update-wiki.sh
 
 source bin/credentials-bothelp.sh
 
 set -x
 
+SERVER_ENV=beta \
+BOTNAME=bothelp \
+BOT_APP_HOST=bot.freecodecamp.com \
 GITTER_USER_TOKEN=${GITTER_USER_TOKEN} \
-    GITTER_APP_KEY=${GITTER_APP_KEY} \
-    GITTER_APP_SECRET=${GITTER_APP_SECRET} \
-    LOG_LEVEL=10 \
-    PORT=7891 \
-    BOT_APP_HOST=bot.freecodecamp.com \
-    BOTNAME=bothelp \
-    SERVER_ENV=beta \
+GITTER_APP_KEY=${GITTER_APP_KEY} \
+GITTER_APP_SECRET=${GITTER_APP_SECRET} \
+LOG_LEVEL=10 \
+PORT=7891 \
     nodemon -x iojs app.js
 
     # nodemon --harmony app.js
