bca660435a5fe8fb466ca77f707586c913609516
dcsan <dc@pikkle.com>
2015-08-02 13:01:00 -0700

    pm2up

--- nap/bin/pm2-update.sh
+++ nap/bin/pm2-update.sh
@@ -2,5 +2,7 @@
 set -x
 
 approot="/home/freecodecamp/www/gitterbot/nap"
+logpath="${approot}/logs/bot.log"
 
-git pull && pm2 restart all && pm2 logs > "${approot}/logs/bot.log"
+git pull && pm2 restart all && pm2 logs > $logpath &&
+tail -f $logpath
