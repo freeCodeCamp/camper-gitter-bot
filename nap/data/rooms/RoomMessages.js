e5bb5406cd4e1c7867e017f5c6b7a4bab1e4d6b2
dcsan <dc@pikkle.com>
2015-08-02 18:04:04 -0700

    room messages first pass

---
+++ nap/data/rooms/RoomMessages.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// using js rather than json so we can have functions and comments
+
+var Utils = require('../../lib/utils/Utils');
+var _ = require('lodash-node');
+
+var SharedMessages = {
+    bonfireChat: {
+        word: 'bonfire',
+            // text: "> we have bonfire specific chatroom here [FreeCodeCamp/HelpBonfires](https://gitter.im/FreeCodeCamp/HelpBonfires)",
+            text: "> type `bonfire name` \n> to get some info on that bonfire here",
+            chance: 0.5
+    },
+    meteorChat: {
+        word: 'meteor',
+            text: "> we have a meteor channel here: [camperbot/meteorJS](https://gitter.im/camperbot/meteorJS)",
+            chance: 0.2
+    }
+};
+
+var RoomMessages = {
+
+    rooms: {
+        'camperbot/testing': [
+            {
+                word: 'holler',
+                text: "> holler back!",
+                chance: 1
+            }
+        ],
+
+        'camperbot/localdev': [
+            {
+                word: 'holler',
+                text: "> holler back!",
+                chance: 1
+            },
+            SharedMessages.bonfireChat,
+            SharedMessages.meteorChat
+        ],
+
+        'freecodecamp/help': [
+            SharedMessages.bonfireChat,
+            SharedMessages.meteorChat
+        ],
+
+        'freecodecamp/helpbonfires': [
+            SharedMessages.bonfireChat,
+            SharedMessages.meteorChat
+        ],
+
+    },
+
+    scanInput: function(input, room, chance) {
+        if (Math.random() > chance) {
+            return null;   // dont always reply
+        }
+        var res;
+        var chat = input.message.model.text.toLowerCase();
+        chance = chance || 1;
+        room = room.toLowerCase();
+        var checkList = this.rooms[room];
+        if (!checkList) { return false; }
+        var msgList = checkList.filter(function(item) {
+            if(!item) { return null; }
+            var flag = (chat.includes(item.word));
+            Utils.tlog(chat, item.word, "flag:" + flag);
+            return flag;
+        });
+        if (msgList.length > 0) {
+            // TODO order by 'chance' and pick highest
+            //Utils.log('msgList', msgList);
+            res = _.sample(msgList);
+            if (Math.random() < (res.chance || 1)) {
+                return res.text;
+            }
+        } else {
+            return null;
+        }
+
+    }
+
+};
+
+module.exports = RoomMessages;
---
+++ nap/test/RoomMessagesSpec.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var expect = require("chai").expect;
+
+var RoomMessages = require("../data/rooms/RoomMessages.js");
+var TestHelper = require("./TestHelper.js");
+
+
+describe("RoomMessages", function(){
+
+    it("should find a message for ", function() {
+        var chat = "you gotta holler i say";
+        var input = TestHelper.makeInputFromString(chat);
+        var msg = RoomMessages.scanInput(input, "camperbot/testing", 1);
+        expect(msg).to.equal("> holler back!");
+    });
+
+    it("should be silent in 0 chance rooms", function() {
+        var chat = "you gotta holler i say";
+        var input = TestHelper.makeInputFromString(chat);
+        var msg = RoomMessages.scanInput(input, "camperbot/testing", 0);
+        expect(msg).to.equal(false);
+    });
+
+
+
+});
+
