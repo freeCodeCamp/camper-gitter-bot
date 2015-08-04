ceddbd6791607fa8109084780b22cd41f6958aae
dcsan <dc@pikkle.com>
2015-08-03 21:17:45 -0700

    config

--- nap/bin/run-bothelp.sh
+++ nap/bin/run-bothelp.sh
@@ -2,16 +2,17 @@
 
 # bin/update-wiki.sh
 
-source bin/credentials-bothelp.sh
+#source bin/credentials-bothelp.sh
 
-set -x
+#set -x
+#
+#SERVER_ENV=local \
+#GITTER_USER_TOKEN=${GITTER_USER_TOKEN} \
+#GITTER_APP_KEY=${GITTER_APP_KEY} \
+#GITTER_APP_SECRET=${GITTER_APP_SECRET} \
+#LOG_LEVEL=10 \
+#PORT=7891 \
 
-SERVER_ENV=local \
-GITTER_USER_TOKEN=${GITTER_USER_TOKEN} \
-GITTER_APP_KEY=${GITTER_APP_KEY} \
-GITTER_APP_SECRET=${GITTER_APP_SECRET} \
-LOG_LEVEL=10 \
-PORT=7891 \
-    nodemon -x iojs app.js
+nodemon -x iojs app.js
 
     # nodemon --harmony app.js
