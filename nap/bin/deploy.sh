442e851f2d053c1915e008487d29fa0d6173496c
dcsan <dc@pikkle.com>
2015-08-03 21:47:30 -0700

    wiki update and merge

--- nap/bin/deploy.sh
+++ nap/bin/deploy.sh
@@ -4,6 +4,6 @@ set -x
 bin/wiki-update.sh
 ssh freecodecamp@104.131.2.16 "cd /home/freecodecamp/www/gitterbot/nap && \
     git clean -f && \
-    git checkout . \
-    && git pull && \
+    git checkout . && \
+    git pull && \
     bin/pm2-update.sh"
