0f73b975c7e69c90b5beca3a3ac0f921789fbaa5
Abhisek Pattnaik <crownabhisek007@gmail.com>
2015-08-15 11:50:10 +0530

    Thanks regex updated
    Updated regex to match thanks, thanx, tx, thank, thx

--- nap/data/rooms/RoomMessages.js
+++ nap/data/rooms/RoomMessages.js
@@ -44,7 +44,7 @@ var AllRoomMessages = [
         chance: 1   // only say this 50% of the time
     },
     {
-        regex: /\btha?n?[xk]s?\b/gim,
+        regex: /\bth?a?n?[xk]s?q?\b/gim,
         func: BotCommands.thanks
     }
 ]
