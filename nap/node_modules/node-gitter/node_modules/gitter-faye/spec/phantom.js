9b0da8077ebd6c81d1d2746f2f5a69c5c307bf90
SaintPeter <rex.schrader@gmail.com>
2015-11-22 16:54:46 -0800

    Remove node_modules from repo

--- nap/node_modules/node-gitter/node_modules/gitter-faye/spec/phantom.js
+++
@@ -1,6 +0,0 @@
-phantom.injectJs('node_modules/jstest/jstest.js')
-
-var options  = {format: 'dot'},
-    reporter = new JS.Test.Reporters.Headless(options)
-
-reporter.open('spec/browser.html')
--- nap/node_modules/node-gitter/node_modules/gitter-faye/spec/ruby/engine/memory_spec.rb
+++
@@ -1,6 +0,0 @@
-require "spec_helper"
-
-describe Faye::Engine::Memory do
-  let(:engine_opts)  { {:type => Faye::Engine::Memory} }
-  it_should_behave_like "faye engine"
-end
