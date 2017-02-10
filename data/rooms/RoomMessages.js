d23d17eff92c8859700597998a4e68a65df09a31
MANISH-GIRI <manish.giri.me@gmail.com>
2017-02-10 17:56:21 -0500

    allow dots and commas before thanks

--- data/rooms/RoomMessages.js
+++ data/rooms/RoomMessages.js
@@ -46,7 +46,7 @@ const AllRoomMessages = [
   {
     // tests: https://regex101.com/r/hH5cN7/42
     // eslint-disable-next-line max-len
-    regex: /((?:^|\s)(?:(?:th(?:n[qx]|x)|t[xyq]|tn(?:[x]){0,2})|than[kx](?:[sxz]){0,2}(?:[uq]|y(?:ou)?)?)|grazie|arigato(?:[u]{0,1})|doumo|gracias?|spasibo|dhanyavaad(?:hamulu)?|o?brigad(?:o|a)|dziekuje|(?:re)?merci|multumesc|shukra?an|danke)\b/gi,
+    regex: /((?:^|\s)(?:(?:th(?:n[qx]|x)|t[xyq]|tn(?:[x]){0,2})|\w*\s*[\.,]*\s*than[kx](?:[sxz]){0,2}|than[kx](?:[sxz]){0,2}(?:[uq]|y(?:ou)?)?)|grazie|arigato(?:[u]{0,1})|doumo|gracias?|spasibo|dhanyavaad(?:hamulu)?|o?brigad(?:o|a)|dziekuje|(?:re)?merci|multumesc|shukra?an|danke)\b/gi,
     func: BotCommands.thanks
   },
   {
